0b652d516deca86ce51429b1f56fb510e04315cf
JAYash9 <jayash.shrivastav@gmail.com>
2025-03-15 19:23:11 +0530

    changes

--- sp/main.py
+++ sp/main.py
@@ -11,6 +11,11 @@ for i in filepath:
     pdf.add_page()
     pdf.set_font("Arial", size=16, style="B")
     pdf.cell(w=50, h=8, txt=f"{name}", ln=1)
+    with open(i, "r") as file:
+        content=file.read()
+    pdf.set_font("Arial", size=12)
+    pdf.multi_cell(w=0, h=6, txt=content)
   
+
 pdf.output("Multiple_pdf.pdf")
 
